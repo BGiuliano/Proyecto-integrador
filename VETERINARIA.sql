d4c937a0b737ceb33ca6347f70a659cfadec5aa8
Pablo Barrera <jabesbarreraa@gmail.com>
2022-08-11 11:32:24 -0300

    agregar perro

--- VETERINARIA.sql
+++ VETERINARIA.sql
@@ -56,4 +56,7 @@ INSERT INTO Historial values
 ('011','2022/07/21', '1','Revisión de control y pipetas','4000'), ('012','2021/06/11', '4','Vacunas','2000'), 
 ('013','2012/08/11', '8','Baño y corte','2000'),('014','2022/08/07', '11','Baño y corte','3500');
 
+INSERT INTO Perro values 
+('013','Troya','2020/08/22','Hembra','39724184');
+SELECT * FROM Perro;
 
