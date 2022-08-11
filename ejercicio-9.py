e8618303b1debb0e1e443ad55da0e56f6b3ea7bb
Roberto <rpgrca@gmail.com>
2022-08-10 21:30:42 -0300

    add comment with exercise description

--- ejercicio-9.py
+++ ejercicio-9.py
@@ -1,3 +1,10 @@
+# 9) Crear una tupla en Python con el nombre de “Historial5” la cual contenga
+#    los siguientes valores:
+#    8520, 9510, 7530, 3570, 1590
+#    Que representa montos de atención en pesos por diferentes servicios/
+#    consultas de la mascota “Toto”. Crear una función para determinar el
+#    valor máximo de atención gastada en “Toto”.
+
 def valor_maximo_gastado(valores):
     return max(valores)
 
