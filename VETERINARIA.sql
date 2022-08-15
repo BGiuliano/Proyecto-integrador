fda12d833cc255d99d6dc6dc178476b3f3755ef8
Roberto <rpgrca@gmail.com>
2022-08-14 22:43:09 -0300

    convert to int, iso date

--- VETERINARIA.sql
+++ VETERINARIA.sql
@@ -35,17 +35,17 @@ foreign key(Perro) references Perro(ID_Perro)
 );
 
 INSERT INTO Dueno values
-('39724184','Mariquena','Allosa','3765897654','Felix Bogado 234'),('22587645','Marcela','Corin','3765986782','Fco de Haro 235'),
-('42356749','Aldana','Gonzalez','3765896543','Colon 3234'),('20053318','Daniel','Lopez','3765897654','Lavalle 765'),
-('4567875','Maria','Lopez','1123457865','Colon 3425'),('20456723','Pedro','Gomez','2653785432','Brasil 2425') ;
+(39724184,'Mariquena','Allosa','3765897654','Felix Bogado 234'),(22587645,'Marcela','Corin','3765986782','Fco de Haro 235'),
+(42356749,'Aldana','Gonzalez','3765896543','Colon 3234'),(20053318,'Daniel','Lopez','3765897654','Lavalle 765'),
+(4567875,'Maria','Lopez','1123457865','Colon 3425'),(20456723,'Pedro','Gomez','2653785432','Brasil 2425') ;
 
 INSERT INTO Perro(Nombre, Fecha_nac, Sexo, DNI_dueno) values 
-('Nino','2000-05-21','Macho','39724184'),('Kob','2004-12-13','Macho','22587645'),
-('Lito','2005-12-25','Macho','42356749'),('Frida','2011-11-01','Hembra','20053318'),
-('Lola','2015-12-01','Hembra','4567875'),('toby','2000-12-05','Macho','39724184'),
-('Koby','2010-04-15','Macho','22587645'),('Litio','2018-07-05','Macho','42356749'),
-('Lila','2022-04-15','Hembra','20053318'),('Indigo','2000-06-05','Hembra','4567875'),
-('India','2020-05-05','Hembra','20456723');
+('Nino','2000-05-21','Macho',39724184),('Kob','2004-12-13','Macho',22587645),
+('Lito','2005-12-25','Macho',42356749),('Frida','2011-11-01','Hembra',20053318),
+('Lola','2015-12-01','Hembra',4567875),('toby','2000-12-05','Macho',39724184),
+('Koby','2010-04-15','Macho',22587645),('Litio','2018-07-05','Macho',42356749),
+('Lila','2022-04-15','Hembra',20053318),('Indigo','2000-06-05','Hembra',4567875),
+('India','2020-05-05','Hembra',20456723);
 
 INSERT INTO Historial(Fecha, Perro, Descripcion, Monto) values 
 ('2000-05-05', 1,'Revisión de control',2000),('2020-12-05', 2,'Desparasitacion',3000),
@@ -56,9 +56,9 @@ INSERT INTO Historial(Fecha, Perro, Descripcion, Monto) values
 ('2022-07-21', 1,'Revisión de control y pipetas',4000), ('2021-06-11', 4,'Vacunas',2000), 
 ('2012-08-11', 8,'Baño y corte',2000),('2022-08-07', 11,'Baño y corte',3500);
 
-INSERT INTO Dueno values ('26781887','Alfonso','Roberto','541162033759','21 2975');
-INSERT INTO Perro values ('012','Rin','2020/01/12','Macho','26781887');
+INSERT INTO Dueno values (26781887,'Alfonso','Roberto','541162033759','21 2975');
+INSERT INTO Perro values ('012','Rin','2020-01-12','Macho',26781887);
 
 -- 4. Actualice la fecha de nacimiento de algún animal (perro) que usted considere.
-UPDATE Perro SET Fecha_nac = '2019/01/12' WHERE ID_Perro = 12;
+UPDATE Perro SET Fecha_nac = '2019-01-12' WHERE ID_Perro = 12;
 
